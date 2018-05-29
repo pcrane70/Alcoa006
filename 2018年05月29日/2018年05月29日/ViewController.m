e215d72cec786b3815107cf5eb8e93f10c36e99f
Guo Xuejin <1095435926@qq.com>
2018-05-29 08:54:09 +0800

    第一次更新。 在viewController.h 文件中更新了几行感谢的话， 感谢夜半醒君教会我使用GitHub管理代码。

--- 2018年05月29日/2018年05月29日/ViewController.m
+++ 2018年05月29日/2018年05月29日/ViewController.m
@@ -5,6 +5,10 @@
 //  Created by 郭学进 on 2018/05/29.
 //  Copyright © 2018年 郭学进. All rights reserved.
 //
+/**
+ 感谢 夜半醒 简书地址：https://www.jianshu.com/u/20fb12489527
+ 这里是夜半醒所写githug管理代码使用教程：https://www.jianshu.com/p/f3cba0a3f1bd
+ */
 
 #import "ViewController.h"
 
